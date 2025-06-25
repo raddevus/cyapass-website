bce741bacab2b87c5119cf602c7bfc688a9d07c1
raddevus <1radeutsch1@gmail.com>
2025-06-25 15:58:24 -0400

    Added two public files that will handle rendering the app, even when it is offline.  That means, even if the web is down you can generate your passwords! Amazinggit status

---
+++ public/serviceworker.js
@@ -0,0 +1,35 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+    caches.open('cya-cache').then(function(cache) {
+        return cache.addAll(
+        [
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+            'https://code.jquery.com/jquery-2.2.3.min.js',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
+            '/custom/main.css',
+            '/custom/app.js',
+            '/custom/cyapass.js',
+            '/custom/sha256.js',
+            'offline.htm',
+            'cyapass.ico',
+            'cyapass.png'
+
+        ]
+        );
+    })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("offline.htm");
+        }
+      });
+    })
+  );
+});
